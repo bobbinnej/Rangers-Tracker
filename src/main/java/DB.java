e4ba34282eddf9dbf439def8b2b47e4922bf90b1
jackie <bobbinnej@gmail.com>
2022-05-07 22:01:17 +0300

    Add jdbc4 driver

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,2 +1,5 @@
+import org.sql2o.Sql2o;
+
 public class DB {
+    public static Sql2o sql2o= new Sql2o("jdbc:postgresql://ec2-44-195-169-163.compute-1.amazonaws.com:5432/d8ducgke7pn0h7", "jspamliwjntrut", "198311fc1649fe91835cd6188e7f0c2af27f85763a1f5b9206b7f41421248247");
 }
