a42c3bed5ffd05fd382cef8c86219912012c67a9
jackie <bobbinnej@gmail.com>
2022-05-08 20:17:34 +0300

    Create homepage together with its navigation links

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,18 @@
+import static spark.Spark.*;
+import java.util.HashMap;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.ModelAndView;
+import java.util.Map;
+
+import spark.ModelAndView;
+
 public class App {
+    public static void main(String[] args) {
+      staticFileLocation("/public");
+
+      get("/", (request,respond)->{
+          Map<String, Object>model=new HashMap<>();
+          return new ModelAndView(model,"index.hbs");
+      },  new HandlebarsTemplateEngine());
+    }
 }
