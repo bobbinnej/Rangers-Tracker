4d3988da228e76705717862081e559ed11790622
jackie <bobbinnej@gmail.com>
2022-05-11 10:31:07 +0300

    Debug 500 errors

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DB {
-    public static Sql2o sql2o=new Sql2o("jdbc:postgresql://localhost:5432/wildlife", "postgres","1870691bbn");
-    // public static Sql2o sql2o= new Sql2o("jdbc:postgresql://ec2-44-195-169-163.compute-1.amazonaws.com:5432/d8ducgke7pn0h7", "jspamliwjntrut", "198311fc1649fe91835cd6188e7f0c2af27f85763a1f5b9206b7f41421248247");
+   // public static Sql2o sql2o=new Sql2o("jdbc:postgresql://localhost:5432/wildlife", "postgres","1870691bbn");
+     public static Sql2o sql2o= new Sql2o("jdbc:postgresql://ec2-52-200-215-149.compute-1.amazonaws.com:5432/dd5v66dq2lk0ct", "fhznvrykbabvmk", "76f1f40e953174e489faa5da272c6e7dfee0171eeaaaa59820b663c688a5b973");
 
     public static void main(String[] args) {
         try(Connection con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/wildlife", "postgres", "1870691bbn")){
